ad3f08897456bde3b2fcfec4aaaedfc22abf7aeb
Mark Hustad <hustada80@gmail.com>
2017-09-05 07:32:08 -0700

    mystatetoprops

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import { Header } from './components/common'
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 
 //import reducers and pass into CreateStore
@@ -12,6 +13,7 @@ const App = () => {
 		<Provider store={createStore(reducers)}>
 			<View>
 				<Header headerText="Tech Stack" />
+				<LibraryList />
 			</View>
 		</ Provider>
 	);
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
 	render() {
+		console.log(this.props);
 		return;
 	}
 }
 
+//take global state object and map it to Library list as props
+const mapStateToProps = state => {
+	return { libraries: state.libraries };
+};
 
 // Call connect which returns another function. Immediately call return function with library list.
-export default connect()(LibraryList);
+export default connect(mapStateToProps)(LibraryList);
