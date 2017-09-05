f58d438a5a5aa5dd61b4c81c76a2a45119fcd23b
Mark Hustad <hustada80@gmail.com>
2017-09-05 06:55:44 -0700

    create JSON file to import data to LibraryReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 
 // create reducer with libraries object that always returns an array
+
+//assign LibraryReducer to 'libraries'
+
+// so console.log(store.getState());
+// { libraries: [] }
+
 export default combineReducers({
-	libraries: () => []
+	libraries: LibraryReducer
 });
